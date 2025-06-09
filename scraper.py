8ed2da35735e97cdb0ae7cc21b058fd9ed09de1a
migueltower <106282745+migueltower@users.noreply.github.com>
2025-06-09 09:44:17 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,7 +14,7 @@ sheet = spreadsheet.sheet1
 year = 2024
 prefix = f"CR{year}-"
 start = 0
-end = 999999
+end = 500000
 case_numbers = [f"{prefix}{str(i).zfill(6)}" for i in range(start, end + 1)]
 urls = [f'https://www.superiorcourt.maricopa.gov/docket/CriminalCourtCases/caseInfo.asp?caseNumber={case}' for case in case_numbers]
 
